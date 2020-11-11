3dd376e778cff7e3136f0b50667fe9c9e5c53a75
mohammadmehdi khajehzadeh <mohammad.mehdi.khz@gmail.com>
2020-11-11 19:51:53 +0330

    the list fragment ==> show the play list (contain music and artist name)

---
+++ app/src/main/java/com/example/musicplayer/Fragments/ListFragment.java
@@ -0,0 +1,31 @@
+package com.example.musicplayer.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.musicplayer.R;
+
+
+public class ListFragment extends Fragment {
+
+
+    public ListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view  = inflater.inflate(R.layout.fragment_list, container, false);
+
+        return view;
+    }
+
+}
