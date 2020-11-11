b5ac90fa0f06c2027f4415e95e6553e9cf8b167d
mohammadmehdi khajehzadeh <mohammad.mehdi.khz@gmail.com>
2020-11-11 14:08:12 +0330

    1. action bar removed 2. waiting is added to see the splash screen full

--- app/src/main/java/com/example/musicplayer/SplashScreenActivity.java
+++ app/src/main/java/com/example/musicplayer/SplashScreenActivity.java
@@ -1,8 +1,11 @@
 package com.example.musicplayer;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        // remove the action bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        // set waiting for splash screen.....
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this , HomeActivity.class));
+                finish();
+            }
+        } , 2650);
+
+
     }
 }
