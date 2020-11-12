72671b7e8430aae3cddf0fd19499fddb3deab3fd
mohammadmehdi khajehzadeh <mohammad.mehdi.khz@gmail.com>
2020-11-12 19:24:13 +0330

    1.action on the tabs 2.when app lunche=> show the list music(start)

--- app/src/main/java/com/example/musicplayer/HomeActivity.java
+++ app/src/main/java/com/example/musicplayer/HomeActivity.java
@@ -1,14 +1,56 @@
 package com.example.musicplayer;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.musicplayer.Fragments.ListFragment;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabReselectListener;
+
 public class HomeActivity extends AppCompatActivity {
+
+    BottomBar navigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        InitViews();
+        Start();
+        ViewsController();
+
+
+    }
+
+    private void InitViews() {
+        navigation = findViewById(R.id.bottomBar);
+
+    }
+
+    private void ViewsController() {
+        navigation.setOnTabReselectListener(new OnTabReselectListener() {
+            @Override
+            public void onTabReSelected(int tabId) {
+                if (tabId == R.id.tab_list) {
+                    ListFragment listFragment = new ListFragment();
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.frameLayout, listFragment);
+                    fragmentTransaction.commit();
+                }
+            }
+        });
+    }
+
+    private void Start() {
+        ListFragment listFragment = new ListFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, listFragment);
+        fragmentTransaction.commit();
     }
 }
